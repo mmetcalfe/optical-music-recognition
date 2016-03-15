fc86a57be17d9d7eae2194e5d165da896f6715f0
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2016-03-16 01:03:45 +1100

    Remove audio from ffmpeg example

--- example/demuxing_decoding.c
+++ example/demuxing_decoding.c
@@ -1,6 +1,6 @@
 /*
 Build with:
-    $clang -o ffmpegtest -Wall demuxing_decoding.c -D__STDC_CONSTANT_MACROS -lavdevice -lavformat -lavfilter -lavcodec -lswscale -lavutil && ./ffmpegtest "HD Pro Webcam C920:" out.mpg out.mp3
+    $clang -o ffmpegtest -Wall demuxing_decoding.c -D__STDC_CONSTANT_MACROS -lavdevice -lavformat -lavfilter -lavcodec -lswscale -lavutil && ./ffmpegtest "HD Pro Webcam C920:" out.mpg
 */
 
 /*
@@ -40,25 +40,22 @@ Build with:
 #include <libavformat/avformat.h>
 
 static AVFormatContext *fmt_ctx = NULL;
-static AVCodecContext *video_dec_ctx = NULL, *audio_dec_ctx;
+static AVCodecContext *video_dec_ctx = NULL;
 static int width, height;
 static enum AVPixelFormat pix_fmt;
-static AVStream *video_stream = NULL, *audio_stream = NULL;
+static AVStream *video_stream = NULL;
 static const char *src_filename = NULL;
 static const char *video_dst_filename = NULL;
-static const char *audio_dst_filename = NULL;
 static FILE *video_dst_file = NULL;
-static FILE *audio_dst_file = NULL;
 
 static uint8_t *video_dst_data[4] = {NULL};
 static int      video_dst_linesize[4];
 static int video_dst_bufsize;
 
-static int video_stream_idx = -1, audio_stream_idx = -1;
+static int video_stream_idx = -1;
 static AVFrame *frame = NULL;
 static AVPacket pkt;
 static int video_frame_count = 0;
-static int audio_frame_count = 0;
 
 /* Enable or disable frame reference counting. You are not supposed to support
  * both paths in your application but pick the one most appropriate to your
@@ -111,36 +108,6 @@ static int decode_packet(int *got_frame, int cached)
 
             /* write to rawvideo file */
             fwrite(video_dst_data[0], 1, video_dst_bufsize, video_dst_file);
-        }
-    } else if (pkt.stream_index == audio_stream_idx) {
-        /* decode audio frame */
-        ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, &pkt);
-        if (ret < 0) {
-            fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));
-            return ret;
-        }
-        /* Some audio decoders decode only part of the packet, and have to be
-         * called again with the remainder of the packet data.
-         * Sample: fate-suite/lossless-audio/luckynight-partial.shn
-         * Also, some decoders might over-read the packet. */
-        decoded = FFMIN(ret, pkt.size);
-
-        if (*got_frame) {
-            size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
-            printf("audio_frame%s n:%d nb_samples:%d pts:%s\n",
-                   cached ? "(cached)" : "",
-                   audio_frame_count++, frame->nb_samples,
-                   av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
-
-            /* Write the raw audio data samples of the first plane. This works
-             * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
-             * most audio decoders output planar audio, which uses a separate
-             * plane of audio samples for each channel (e.g. AV_SAMPLE_FMT_S16P).
-             * In other words, this code will write only the first audio channel
-             * in these cases.
-             * You should use libswresample or libavfilter to convert the frame
-             * to packed data. */
-            fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
         }
     }
 
@@ -225,7 +192,7 @@ int main (int argc, char **argv)
 {
     int ret = 0, got_frame;
 
-    if (argc != 4 && argc != 5) {
+    if (argc != 3 && argc != 4) {
         fprintf(stderr, "usage: %s [-refcount] input_file video_output_file audio_output_file\n"
                 "API example program to show how to read frames from an input file.\n"
                 "This program reads frames from a file, decodes them, and writes decoded\n"
@@ -237,13 +204,12 @@ int main (int argc, char **argv)
                 "\n", argv[0]);
         exit(1);
     }
-    if (argc == 5 && !strcmp(argv[1], "-refcount")) {
+    if (argc == 4 && !strcmp(argv[1], "-refcount")) {
         refcount = 1;
         argv++;
     }
     src_filename = argv[1];
     video_dst_filename = argv[2];
-    audio_dst_filename = argv[3];
 
     /* register all formats and codecs */
     avdevice_register_all();
@@ -297,22 +263,11 @@ int main (int argc, char **argv)
         video_dst_bufsize = ret;
     }
 
-    if (open_codec_context(&audio_stream_idx, fmt_ctx, AVMEDIA_TYPE_AUDIO) >= 0) {
-        audio_stream = fmt_ctx->streams[audio_stream_idx];
-        audio_dec_ctx = audio_stream->codec;
-        audio_dst_file = fopen(audio_dst_filename, "wb");
-        if (!audio_dst_file) {
-            fprintf(stderr, "Could not open destination file %s\n", audio_dst_filename);
-            ret = 1;
-            goto end;
-        }
-    }
-
     /* dump input information to stderr */
     av_dump_format(fmt_ctx, 0, src_filename, 0);
 
-    if (!audio_stream && !video_stream) {
-        fprintf(stderr, "Could not find audio or video stream in the input, aborting\n");
+    if (!video_stream) {
+        fprintf(stderr, "Could not find video stream in the input, aborting\n");
         ret = 1;
         goto end;
     }
@@ -331,8 +286,6 @@ int main (int argc, char **argv)
 
     if (video_stream)
         printf("Demuxing video from file '%s' into '%s'\n", src_filename, video_dst_filename);
-    if (audio_stream)
-        printf("Demuxing audio from file '%s' into '%s'\n", src_filename, audio_dst_filename);
 
     /* read frames from the file */
     int frame_index = 0;
@@ -369,37 +322,11 @@ int main (int argc, char **argv)
                video_dst_filename);
     }
 
-    if (audio_stream) {
-        enum AVSampleFormat sfmt = audio_dec_ctx->sample_fmt;
-        int n_channels = audio_dec_ctx->channels;
-        const char *fmt;
-
-        if (av_sample_fmt_is_planar(sfmt)) {
-            const char *packed = av_get_sample_fmt_name(sfmt);
-            printf("Warning: the sample format the decoder produced is planar "
-                   "(%s). This example will output the first channel only.\n",
-                   packed ? packed : "?");
-            sfmt = av_get_packed_sample_fmt(sfmt);
-            n_channels = 1;
-        }
-
-        if ((ret = get_format_from_sample_fmt(&fmt, sfmt)) < 0)
-            goto end;
-
-        printf("Play the output audio file with the command:\n"
-               "ffplay -f %s -ac %d -ar %d %s\n",
-               fmt, n_channels, audio_dec_ctx->sample_rate,
-               audio_dst_filename);
-    }
-
 end:
     avcodec_close(video_dec_ctx);
-    avcodec_close(audio_dec_ctx);
     avformat_close_input(&fmt_ctx);
     if (video_dst_file)
         fclose(video_dst_file);
-    if (audio_dst_file)
-        fclose(audio_dst_file);
     av_frame_free(&frame);
     av_free(video_dst_data[0]);
 
