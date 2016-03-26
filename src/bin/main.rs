d4c2503558e0a4548c677cf959fb8899ecffb558
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2016-03-26 16:19:03 +1100

    Display webcam images as a texture in OpenGL

---
+++ src/bin/main.rs
@@ -0,0 +1,116 @@
+#[macro_use]
+extern crate glium;
+extern crate image;
+
+extern crate optical_music_recognition;
+use optical_music_recognition::ffmpeg_camera::ffmpeg_camera;
+
+// use std::io::Cursor;
+use glium::DisplayBuild;
+use glium::Surface;
+
+use std::borrow::Cow;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+implement_vertex!(Vertex, position, tex_coords);
+
+fn main() {
+    let mut camera =
+        ffmpeg_camera::FfmpegCamera::get_default()
+            .expect("Failed to open camera.");
+
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+
+    // let image = image::load(Cursor::new(&include_bytes!("../../curved-3.jpg")[..]),
+    //                         image::JPEG).unwrap().to_rgba();
+    // let image_dimensions = image.dimensions();
+    // let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
+    // let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+
+
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+        uniform mat4 matrix;
+        void main() {
+            v_tex_coords = tex_coords;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        in vec2 v_tex_coords;
+        out vec4 color;
+        uniform sampler2D tex;
+        void main() {
+            color = texture(tex, v_tex_coords);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+     let mut t = -0.5;
+
+    loop {
+        let webcam_frame = camera.get_image().unwrap();
+        let cow: Cow<[_]> = Cow::Owned(webcam_frame.data);
+        // let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
+        let raw_image = glium::texture::RawImage2d {
+            data: cow,
+            width: webcam_frame.width as u32,
+            height: webcam_frame.height as u32,
+            format: glium::texture::ClientFormat::U8U8
+        };
+        let texture = glium::texture::Texture2d::new(&display, raw_image).unwrap();
+
+        // image.save_pgm("image.pgm").unwrap();
+        // image.save_jpeg("image.jpg").unwrap();
+
+        // we update `t`
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [ t , 0.0, 0.0, 1.0f32],
+            ],
+            tex: &texture,
+        };
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                    &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        // listing the events produced by the window and waiting to be received
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,   // the window has been closed by the user
+                _ => ()
+            }
+        }
+    }
+}
