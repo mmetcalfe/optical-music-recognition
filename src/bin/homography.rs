90282512a1ce27a6ff09baf499dd239379724c54
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2016-04-21 19:22:26 +1000

    Allow taking a photo by pressing the space bar

--- src/bin/homography.rs
+++ src/bin/homography.rs
@@ -28,33 +28,43 @@ use std::cmp;
 use time::Duration;
 use time::SteadyTime;
 
+fn get_fake_webcam_frame() -> image_ycbcr::Image {
+    // Fake webcam frame:
+    println!("Fake image:");
+    let file_name = String::from("image_uyvy.jpg");
+    let load_result = af::load_image(file_name, true);
+    if load_result.is_err() {
+        panic!("LOAD FAILED!");
+    }
+    let rgb_image = load_result.unwrap();
+    println!("shape: {}", rgb_image.dims().unwrap());
+    let ycbcr_frame = image_ycbcr::Image::from_af_array(rgb_image);
+    println!("save_jpeg:");
+    // ycbcr_frame.save_jpeg("image_ycbcr.jpg").unwrap();
+
+    ycbcr_frame
+
+    // // Reconstruct webcam frame vector from its af_array.
+    // println!("Sequence:");
+    // let seqs = &[
+    //     af::Seq::default(),
+    //     af::Seq::default(),
+    //     af::Seq::new(0.0, 2.0, 1.0),
+    // ];
+    // let pix_array = af::index(&ycbcr_frame.af_data, seqs).unwrap();
+    // let pix_array = pix_array.cast::<f32>().unwrap();
+    // println!("from_af_array:");
+    // let ycbcr_frame = image_ycbcr::Image::from_af_array(pix_array);
+}
+
 fn main() {
     // af::set_device(0);
     // af::info();
     println!("Available ArrayFire backends: {:?}", af::get_available_backends());
 
-    // println!("Join test:");
-    // let values = [
-    //     1.0, 2.0,   3.0, 4.0,    5.0,  6.0,
-    //     7.0, 8.0,   9.0, 10.0,  11.0, 12.0,
-    //     13.0, 14.0,  15.0, 16.0,  17.0, 18.0,
-    // ];
-    // let test_array = af::Array::new::<f32>(&values, af::Dim4::new(&[2, 3, 3, 1])).unwrap();
-    // af::print(&test_array);
-    // let seqs = &[
-    //     af::Seq::new(0.0, 1.0, 1.0),
-    //     af::Seq::new(0.0, 0.0, 1.0),
-    //     af::Seq::new(0.0, 0.0, 1.0),
-    // ];
-    // let pix_array = af::index(&test_array, seqs).unwrap();
-    // af::print(&pix_array);
-    //
-    // return;
-    //
-
     // let (img_w, img_h) = (320, 240);
-    // let (img_w, img_h) = (640, 480);
-    let (img_w, img_h) = (1280, 720);
+    let (img_w, img_h) = (640, 480);
+    // let (img_w, img_h) = (1280, 720);
     // let (img_w, img_h) = (1920, 1080);
 
     let mut camera = ffmpeg_camera::FfmpegCamera::get_best((img_w, img_h))
@@ -75,21 +85,8 @@ fn main() {
 
     let mut frame_start_time = SteadyTime::now();
 
-    // // Fake webcam frame:
-    // println!("Fake image:");
-    // let file_name = String::from("image_uyvy.jpg");
-    // let load_result = af::load_image(file_name, true);
-    // if load_result.is_err() {
-    //     println!("LOAD FAILED!");
-    //     return;
-    // }
-    // let rgb_image = load_result.unwrap();
-    // println!("shape: {}", rgb_image.dims().unwrap());
-    // let ycbcr_frame = image_ycbcr::Image::from_af_array(rgb_image);
-    // println!("save_jpeg:");
-    // // ycbcr_frame.save_jpeg("image_ycbcr.jpg").unwrap();
-
-    // return;
+    let mut captured_frame: Option<image_ycbcr::Image> = None;
+    let mut take_photo = false;
 
     loop {
     // {
@@ -97,72 +94,44 @@ fn main() {
         let webcam_frame = camera.get_image_uyvy().unwrap();
         // webcam_frame.save_jpeg("image_uyvy.jpg").unwrap();
         let ycbcr_frame = draw_ctx.convert_uyvy_ycbcr(&webcam_frame).unwrap();
+        // // Fake webcam frame:
+        // let ycbcr_frame = get_fake_webcam_frame();
 
-        // println!("Sequence:");
-        // let seqs = &[
-        //     af::Seq::default(),
-        //     af::Seq::default(),
-        //     af::Seq::new(0.0, 2.0, 1.0),
-        // ];
-        // let pix_array = af::index(&ycbcr_frame.af_data, seqs).unwrap();
-        // let pix_array = pix_array.cast::<f32>().unwrap();
-        // println!("from_af_array:");
-        // let ycbcr_frame = image_ycbcr::Image::from_af_array(pix_array);
+        if take_photo {
+            println!("TAKE PHOTO!");
+            ycbcr_frame.save_jpeg("captured_frame.jpg").unwrap();
+            take_photo = false;
 
-        println!("save:");
-        ycbcr_frame.save_jpeg("image_ycbcr.jpg").unwrap();
+            captured_frame = Some(ycbcr_frame);
+            continue;
+        } else {
+            println!("save:");
+            ycbcr_frame.save_jpeg("image_ycbcr.jpg").unwrap();
+        }
+
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
-        draw_ctx.draw_image_ycbcr(&mut target, &ycbcr_frame);
+        // draw_ctx.draw_image_ycbcr(&mut target, &ycbcr_frame);
+        if let Some(ref frame) = captured_frame {
+            draw_ctx.draw_image_ycbcr(&mut target, &frame);
+        }
 
         // Begin ORB detection:
 
         let img_ycbcra = &ycbcr_frame.af_data;
         println!("shape: {}", img_ycbcra.dims().unwrap());
 
-        // Slice:
-        println!("Slice:");
         let img_grey = af::slice(img_ycbcra, 0).unwrap();
         let img_grey = img_grey.cast::<f32>().unwrap();
 
-        // // Colour convert:
-        // println!("Colour convert:");
-        // let seqs = &[
-        //     af::Seq::default(),
-        //     af::Seq::default(),
-        //     // Seq::new(1.0, 3.0, 1.0)
-        //     // af::Seq::new(1.0, 3.0, 1.0),
-        //     // af::Seq::new(1.0, 3.0, 1.0),
-        //     af::Seq::new(0.0, 2.0, 1.0)
-        // ];
-        // let img_ycbcr = af::index(img_ycbcra, seqs).unwrap();
-        // // af::print(&img_ycbcr);
-        // println!("shape: {}", img_ycbcr.dims().unwrap());
-        // let img_grey = af::color_space(&img_ycbcr, af::ColorSpace::GRAY, af::ColorSpace::RGB).unwrap();
-
-        // println!("backend: {:?}", img_grey.get_backend());
-        // println!("shape: {:?}", img_grey.dims().unwrap());
-        // match img_grey.get_type().unwrap() {
-        //     af::Aftype::F32 => println!("type: f32"),
-        //     _ => println!("type: UNKNOWN"),
-        // }
-        // println!("numdims: {:?}", img_grey.numdims().unwrap());
-
-        // let num_rows: u64 = 200;
-        // let num_cols: u64 = 200;
-        // let dims = af::Dim4::new(&[num_rows, num_cols, 1, 1]);
-        // let img_grey = af::randu::<f32>(dims).unwrap();
-
-
-        let fast_thr = 80.0;
-        let max_feat = 50;
-        let scl_fctr = 1.5;
-        let levels = 4;
-        let blur_img = true;
         {
-            println!("orb:");
-            // TODO: Find out why af::orb only succeeds for 320x240 images.
+            let fast_thr = 80.0;
+            let max_feat = 16;
+            let scl_fctr = 1.5;
+            let levels = 4;
+            let blur_img = true;
+
             let orb_result = af::orb(
                 &img_grey,
                 fast_thr,
@@ -172,7 +141,7 @@ fn main() {
                 blur_img
             );
 
-            if let Ok((orb_features, orb_arr)) = orb_result {
+            if let Ok((orb_features, orb_descriptors)) = orb_result {
                 print!("ORB SUCCEDED: ");
                 let num_features = orb_features.num_features().unwrap() as usize;
                 println!("num_features: {:?}", num_features);
@@ -201,9 +170,7 @@ fn main() {
                     println!("{:?}.", err);
                 }
             }
-            println!("End orb scope.");
         }
-        println!("After orb scope.");
 
         let frame_duration = SteadyTime::now() - frame_start_time;
         frame_start_time = SteadyTime::now();
@@ -223,7 +190,13 @@ fn main() {
         // listing the events produced by the window and waiting to be received
         for ev in display.poll_events() {
             match ev {
-                glium::glutin::Event::Closed => return,   // the window has been closed by the user
+                // the window has been closed by the user
+                glium::glutin::Event::Closed => return,
+                glium::glutin::Event::ReceivedCharacter(ch) => {
+                    if ch == ' ' {
+                        take_photo = true;
+                    }
+                },
                 _ => ()
             }
         }
